58c7d627116b8d656eeea71a55f752a5c1936e9a
Mike Goldin <mike.goldin@consensys.net>
2017-10-20 14:13:56 -0400

    Add test for token deposits

--- test/dead.js
+++ test/dead.js
@@ -2,8 +2,10 @@
 /* global assert artifacts contract */
 
 const Dead = artifacts.require('Dead.sol');
+const Token = artifacts.require('./tokens/HumanStandardToken.sol');
 
 const utils = require('./utils.js');
+const BN = require('bignumber.js');
 
 contract('Dead', (accounts) => {
   describe('Function: heartbeat', () => {
@@ -59,10 +61,35 @@ contract('Dead', () => {
   });
 });
 
-contract('Dead', () => {
+contract('Dead', (accounts) => {
   describe('Function: depositERC20', () => {
-    it('should fire an event indicating the amount of tokens deposited, the token\'s ' +
-       'symbol and its address');
+    const [owner, tokenOwner] = accounts;
+
+    it('should fire an event indicating the amount of tokens deposited ' +
+       'and its address', async () => {
+      const dead = await Dead.deployed();
+      const token = await Token.deployed();
+
+      const ownerInitialBalance = await token.balanceOf.call(owner);
+
+      await utils.as(owner, token.transfer, tokenOwner, ownerInitialBalance);
+
+      const tokenOwnerInitialBalance = await token.balanceOf.call(tokenOwner);
+
+      await utils.as(tokenOwner, token.approve, dead.address, tokenOwnerInitialBalance);
+      const receipt = await utils.as(tokenOwner, dead.depositERC20, token.address);
+
+      assert.strictEqual(utils.getReceiptValue(receipt, '_addr').toString(), token.address);
+      assert.strictEqual(utils.getReceiptValue(receipt, '_amount').toString(), '1000');
+
+      const tokenOwnerFinalBalance = await token.balanceOf.call(tokenOwner);
+      assert(tokenOwnerFinalBalance.eq(new BN('0', 10)),
+        'the token owner\'s balance was not properly decremented');
+
+      const deadFinalBalance = await token.balanceOf.call(dead.address);
+      assert(deadFinalBalance.eq(tokenOwnerInitialBalance),
+        'the DMS\' final balance was not properly incremented');
+    });
   });
 });
 
