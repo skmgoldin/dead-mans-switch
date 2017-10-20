2830f02ff1a6e0ad98c445e6b43965d46fbcb93e
Mike Goldin <mike.goldin@consensys.net>
2017-10-20 14:26:07 -0400

    Add token migration

---
+++ migrations/3_token.js
@@ -0,0 +1,7 @@
+/* global artifacts */
+
+const Token = artifacts.require('./tokens/HumanStandardToken.sol');
+
+module.exports = (deployer) => {
+  deployer.deploy(Token, '1000', 'DEAD', '0', 'DED');
+};
