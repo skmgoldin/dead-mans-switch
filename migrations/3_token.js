43ab27d1a3222546f1298d8428205593b608fcef
Mike Goldin <mike.goldin@consensys.net>
2017-10-20 17:56:50 -0400

    Add informative printer

--- migrations/3_token.js
+++ migrations/3_token.js
@@ -4,6 +4,8 @@ const Token = artifacts.require('./tokens/HumanStandardToken.sol');
 
 module.exports = (deployer, network) => {
   if (network === 'test') {
+    console.log('we\'re testing, so we\'ll deploy a fake token');
     deployer.deploy(Token, '1000', 'DEAD', '0', 'DED');
   }
 };
+
