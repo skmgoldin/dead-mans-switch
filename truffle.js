75bc9c8456f909f6419eecd59faa061e444e9f6d
Mike Goldin <mike.goldin@consensys.net>
2017-10-20 17:57:05 -0400

    Add Rinkeby deploy facility

--- truffle.js
+++ truffle.js
@@ -1,3 +1,21 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+let secrets;
+let mnemonic = '';
+
+if (fs.existsSync('secrets.json')) {
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+  mnemonic = secrets.mnemonic;
+}
+
 module.exports = {
-  networks: {},
+  networks: {
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
+      network_id: '*',
+      gas: 4500000,
+      gasPrice: 21000000000,
+    },
+  },
 };
